7e309e32ddcfa1a5ebbf558539016b81b2b09d47
Roman Spirichkin <roman@MacBook-Pro-Roman-Spirichkin.local>
2015-02-28 21:03:17 +0300

    Initial Commit

---
+++ BMA/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BMA
+//
+//  Created by Roman Spirichkin on Feb/28/15.
+//  Copyright (c) 2015 RomanSpirichkinOrganization. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
